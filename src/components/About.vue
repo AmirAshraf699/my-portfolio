45789d64bc911db2c0dce2cdedcd8789306a11e3
amirashraf669 <amirashraf2899@gmail.com>
2025-05-12 05:16:56 +0300

    add new resume

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,32 +1,43 @@
 <template>
-    <div class="about" id="about">
-      <v-container class="justify-center align-center flex-wrap">
-        <img src="@/assets/imgs/My-pic.png" alt="Amir">
-        <div class="content"> 
-          <h2 class="second-title">{{ t('about.title') }}</h2>
-          <div class="text">
-            {{ t('about.text.one') }}
-            <br>
-            {{ t('about.text.two') }}
-            <br>
-            {{ t('about.text.three') }}
-            <br>
-            {{ t('about.text.four') }}
-            <br>
-            {{ t('about.text.five') }}
-            <br>
-            {{ t('about.text.six') }}
-          </div>
-          <div class="info mt-6">
-            <a class="d-inline-block px-3 py-2" href="../pdf/Amir-shraf-Amir.pdf" download="Amir-Ashraf-Amir.pdf" target="_blank">{{ t('about.download') }}</a>
-            <a class="d-inline-block px-3 py-2" href="#contact">{{ t('about.contact') }}</a></div>
+  <div class="about" id="about">
+    <v-container class="justify-center align-center flex-wrap">
+      <img src="@/assets/imgs/My-pic.png" alt="Amir" />
+      <div class="content">
+        <h2 class="second-title">{{ t("about.title") }}</h2>
+        <div class="text">
+          {{ t("about.text.one") }}
+          <br />
+          {{ t("about.text.two") }}
+          <br />
+          {{ t("about.text.three") }}
+          <br />
+          {{ t("about.text.four") }}
+          <br />
+          {{ t("about.text.five") }}
+          <br />
+          {{ t("about.text.six") }}
         </div>
-      </v-container>
-    </div>
+        <div class="info mt-6">
+          <a
+            class="d-inline-block px-3 py-2"
+            :href="resumePdf"
+            download="Amir-Ashraf-Amir-Resume.pdf"
+            target="_blank"
+          >
+            {{ t("about.download") }}
+          </a>
+          <a class="d-inline-block px-3 py-2" href="#contact">{{
+            t("about.contact")
+          }}</a>
+        </div>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
-import { useI18n } from 'vue-i18n';
+import resumePdf from "@/assets/pdf/Amir-shraf-Amir.pdf";
+import { useI18n } from "vue-i18n";
 
-const { t } = useI18n() 
+const { t } = useI18n();
 </script>
